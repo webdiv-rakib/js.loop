5410998b1bd390a816c1619117c7af322dceb8c1
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-02 01:53:15 +0600

    example of for loop

---
+++ for-of.js
@@ -0,0 +1,23 @@
+// Array
+const numbers = [12, 98, 45, 21, 35];
+
+console.log(numbers);
+
+// Loop 
+for (const num of numbers){
+    console.log(num);
+}
+
+const fruites = ['am', 'jam', 'kathal', 'lichu'];
+console.log(fruites);
+
+for (const fol of fruites){
+    console.log(fol);
+}
+
+const roll = [1, 3, 5, 7, 11, 15];
+console.log(roll);
+
+for (const number of roll){
+    console.log(number);
+}
