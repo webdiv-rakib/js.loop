d7c33553aa03319c2bd22bced1e51fcfe4d4d196
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-02 00:56:46 +0600

    js recap

--- recap.js
+++ recap.js
@@ -2,3 +2,16 @@
 // let: value can be change
 // var: we should not use it
 
+// variable
+const amount = 200;
+
+// condition
+if (amount > 100){
+
+}
+else{
+
+}
+
+// array
+const numbers = [12, 23, 35, 45]
