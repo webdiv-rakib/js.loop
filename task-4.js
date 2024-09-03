d3bd28b3871edbb13be73610092fd3091e3a3cbd
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-03 21:17:33 +0600

    task-4 solve

---
+++ task-4.js
@@ -0,0 +1,18 @@
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+
+
+/*programming hero*/
+
+
+let num = 5;
+let multi = 0;
+while (num <= 5){
+    multi = num * 5;
+    console.log(multi);
+
+    num++;
+}
