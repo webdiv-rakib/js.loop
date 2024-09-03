04944d01a7a69ec9c7fad7bb882ee91bf6e294e4
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-03 20:52:56 +0600

    task-1 solve

---
+++ task-1.js
@@ -0,0 +1,13 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+/*programming hero*/
+
+let num = 1;
+while (num <= 60){
+    console.log("I will invest at least 6 hrs every single day for next 60 days!",num);
+    num++;
+}
