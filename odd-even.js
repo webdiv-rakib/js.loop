b5f43862e1c3fcfd27f0035037220b3e2f4a346d
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-03 22:09:55 +0600

    odd even example using for loop

---
+++ odd-even.js
@@ -0,0 +1,13 @@
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+for(let i = 1; i <= 50; i++){
+    console.log(i);
+}
+
+// Even number
+
+for(let i = 0; i <= 20; i = i+2){
+    console.log(i);
+}
