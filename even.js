0305085070c9a74454a2e07edec681587f62682a
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-03 20:34:20 +0600

    finding even number using while loop

--- even.js
+++ even.js
@@ -1,5 +1,8 @@
-const num = 1;
-while (num <= 10){
-    console.log(num);
+let num = 1;
+while (num <= 100){
+    // console.log(num);
+    if(num % 2 == 0){
+        console.log("Even number: ",num);
+    }
     num++;
 }
