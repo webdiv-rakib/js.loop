4f4b9476d47440e11b0bfdc7c3a2db0e85f64572
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-03 22:04:25 +0600

    for loop example

---
+++ for.js
@@ -0,0 +1,20 @@
+// While loop
+let num = 0; //loop variable
+while (num < 5) { //loop condition
+    console.log(num); //loop statement
+    num++; //loop variable change
+}
+
+//------ For Loop structure-------
+
+/**
+ * for (first part ; second part ; third part){
+ * 
+ *  loop statement
+ * }
+ * 
+ */
+
+for (let num = 0; num < 5; num++){
+    console.log(num);
+}
