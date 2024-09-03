d467c5e37b6c164dc88bcbe9e16b811816743639
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-04 00:18:20 +0600

    for-task-4 solve

---
+++ for-task-4.js
@@ -0,0 +1,14 @@
+/***
+
+Generate a multiplication table for number 9
+
+ */
+
+
+/*programming hero*/
+let num = 9;
+let multi = 0;
+for (let i = 1; i <= 10; i++) {
+    multi = num * i;
+    console.log(multi);
+}
