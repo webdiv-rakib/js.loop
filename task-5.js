7fc01d6fe3ded78fdc76d02878dc5020430c19fb
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-03 21:26:56 +0600

    task-5 solve

---
+++ task-5.js
@@ -0,0 +1,15 @@
+/***
+
+Implement a countdown timer that counts down from 21 to 15.
+
+ */
+
+
+/*programming hero*/
+
+let num = 21;
+while(num >= 15){
+    console.log(num);
+    num = num - 1;
+    
+}
