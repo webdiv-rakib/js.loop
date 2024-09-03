4f5abfae5e36601a718875893a0b02db3ae31818
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-03 20:19:56 +0600

    while loop problem solving using

---
+++ range.js
@@ -0,0 +1,11 @@
+let numbers = 1;
+while (numbers <= 10){ //focus on the condition
+    console.log("I Love You Sanzi");
+    numbers++;
+}
+
+let num = 50;
+while (num <=100){
+    console.log(num);
+    num++;
+}
