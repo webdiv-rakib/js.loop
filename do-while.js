375b0272fa3c8b945096a6061c7aa9efe88f0d7a
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-04 17:28:40 +0600

    do while loop example

---
+++ do-while.js
@@ -0,0 +1,17 @@
+// Almost we will not use
+
+// While loop
+let i = 10;
+while (i < 5) {
+    // console.log(i);
+    i++;
+}
+
+// Do while loop
+let n = 10;
+do {
+    console.log(n);
+    n++;
+}
+while (n < 5)
+
