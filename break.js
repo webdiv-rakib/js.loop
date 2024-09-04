d5b863476cb90f1422a95e1c4795940babc06e41
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-04 17:11:16 +0600

    example of using continue

---
+++ break.js
@@ -0,0 +1,16 @@
+// Use of Break
+for (let i = 1; i < 15; i++) {
+    // console.log(i);
+    if (i >= 5) {
+        break;
+    }
+}
+
+let i = 1;
+while (i <= 15) {
+    console.log(i);
+    i++;
+    if (i >= 8) {
+        break;
+    }
+}
---
+++ continue.js
@@ -0,0 +1,9 @@
+// Continue means skip this
+// Break ---> i am done with this loop
+
+for (let i = 1; i < 10; i++) {
+    if (i % 2 == 1) {
+        continue;
+    }
+    console.log(i);
+}
