43169b89b31d53e9296d3b3c7aeca9d95b013ba7
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-04 17:20:55 +0600

    continue use in while loop

--- continue.js
+++ continue.js
@@ -7,3 +7,14 @@ for (let i = 1; i < 10; i++) {
     }
     console.log(i);
 }
+
+// use inside while loop
+
+let n = 0;
+while (n < 50) {
+    n++;
+    if (n % 5 !== 0) {
+        continue;
+    }
+    console.log(n);
+}
